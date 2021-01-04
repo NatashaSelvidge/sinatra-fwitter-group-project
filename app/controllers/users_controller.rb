1af23d1abeb1d65d258b484953d8c1119797f9b9
Natasha Selvidge <Natasha.selvidge@gmail.com>
2021-01-04 09:04:38 -0800

    commit tree 669b0cb462bd8532d5db29108c9fd32a8bbc887f

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,50 @@
 class UsersController < ApplicationController
 
+     get '/login' do
+    if logged_in?
+      redirect to "/tweets"
+    else
+      erb :"users/login"
+    end
+  end
 
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    else
+      redirect to "/signup"
+    end
+  end
+
+get '/signup' do
+    if logged_in?
+      redirect to "/tweets"
+    else
+      erb :"users/create_user"
+    end
+  end
+
+
+  post '/signup' do
+    if params[:username] == "" || params[:email] == "" || params[:password] == ""
+      redirect to "/signup"
+    else
+      user = User.create(params)
+      session[:user_id] = user.id
+      redirect to "/tweets"
+    end
+  end
+
+
+  get '/logout' do
+    if logged_in?
+      session.destroy
+      redirect to '/login'
+    else
+      redirect to '/'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ActiveRecord::Base
+ 
   has_secure_password
-  has_many :tweets
+ 
+  has_many :tweets, through: :user_id
+
+   def slug
+    username.downcase.tr(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
 end
+
